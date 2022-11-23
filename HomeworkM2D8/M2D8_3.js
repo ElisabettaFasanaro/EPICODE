d2c7e72dffc9bcca5cd60376d01d6479de991c96
ElisabettaFasanaro <114584641+ElisabettaFasanaro@users.noreply.github.com>
2022-11-23 14:11:18 +0100

    Update M2D8_3.js

---
+++ HomeworkM2D8/M2D8_3.js
@@ -0,0 +1,29 @@
+//21//
+
+let x ="John";
+let y = "Doe";
+console.log(x, y);
+
+//22//
+
+/*let nome= "Marco";
+let surname= "Pollo";
+let email= "pollomarco@pollo.com";*/
+
+
+
+//23//
+
+//24//
+
+//25//
+
+//26//
+
+//27//
+
+//28//
+
+//29//
+
+//30//
